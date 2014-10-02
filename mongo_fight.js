47d01e5e050fcd83d6ba07842f361ca9190b0fb5
Łukasz Tenerowicz <lukasz.tenerowicz@itsilesia.com>
2014-10-03 01:18:00 +0200

    Added hp, fight states

--- mongo_fight.js
+++ mongo_fight.js
@@ -18,6 +18,7 @@ var characterSchema = mongoose.Schema({
 
 var fightSchema = mongoose.Schema({
     _id: Number,
+    state: String,
     player1: [characterSchema],
     player2: [characterSchema]
 }, { collection: 'mongoFight' });
@@ -34,50 +35,78 @@ var Fight = mongoose.model('Fight', fightSchema)
 
 // Start a TCP Server
 net.createServer(function (socket) {
-    // Handle incoming messages from clients.
+    // When data is received :
     socket.on('data', function (data) {
         try {
             receivedDataObj = JSON.parse(data)
 
-            //TODO: get fight id and player id from input data
+            // get fight id and player id from input data from Unity client
             var fightId = receivedDataObj.fightId
             var playerId = receivedDataObj.playerId
 
-            //TODO: fetch fight with given id from DB
+            var response = null
+
+            // fetch fight with given id from MongoDB
             Fight.findOne({"_id": fightId}, function (err, fightInstance) {
                 if (err) throw err;
 
-                console.log( "Fight instance : " + fightInstance )
+                console.log("Fight instance : " + fightInstance)
 
                 var player = null
                 var enemy = null
+                var fightState = fightInstance.state
 
-                //TODO: determine which player is "player", which is "enemy"
-                if( fightInstance.player1[0].characterId == playerId) {
-                    player = fightInstance.player1[0]
-                    enemy = fightInstance.player2[0]
-                } else if( fightInstance.player2[0].characterId == playerId )  {
-                    player = fightInstance.player2[0]
-                    enemy = fightInstance.player1[0]
+                console.log("Fight state == " + fightInstance.state)
+
+                var response = ""
+
+                //TODO: if gamestate is Waiting for second player then send it to backend
+                if( fightState == "FINISHED" ) {
+                    response = "FINISHED "
+                } else if( fightState == "WAITING" ) {
+                    response = "WAITING "
+                } else {
+                    //TODO: determine which player is "player", which is "enemy"
+                    if( fightInstance.player1[0].characterId == playerId) {
+                        player = fightInstance.player1[0]
+                        enemy = fightInstance.player2[0]
+                    } else if( fightInstance.player2[0].characterId == playerId )  {
+                        player = fightInstance.player2[0]
+                        enemy = fightInstance.player1[0]
+                    }
+
+                    //TODO: actualize player data in DB
+                    player.x = receivedDataObj.newX;
+                    player.y = receivedDataObj.newY;
+                    player.z = receivedDataObj.newZ;
+                    player.yaw = receivedDataObj.yaw;
+                    player.hp = receivedDataObj.hp;
+
+                    var updateQuery = {"_id": fightId};
+                    Fight.findOneAndUpdate(updateQuery, { $set: fightInstance})
+
+                    //TODO: if player or enemy hp is <= 0 then fight is finished
+                    if( 1 == 3 ) {
+
+                    } else {
+                        console.log("Generating response from fightInstance")
+
+                        // Generate response for Unity3D client
+                        var responseArr = [{player: player, enemy: enemy}]
+
+                        response = JSON.stringify(responseArr)
+                    }
                 }
 
-                //TODO: actualize player data in DB
-                player.x = receivedDataObj.newX;
-                player.y = receivedDataObj.newY;
-                player.z = receivedDataObj.newZ;
-                player.yaw = receivedDataObj.yaw;
-
-                var updateQuery = {"_id": fightId};
-                Fight.findOneAndUpdate(updateQuery, { $set: fightInstance})
-
-                response = JSON.stringify(fightInstance)
+                console.log("Response : " + response)
 
                 //TODO: generate response
                 response = response.length + '#' + response;
 
-                console.log("Response : " + response)
+                console.log("Response after length adding")
 
                 socket.write( response )
+
             })
         }catch(err) {
             console.log("Error occured : " + err)
@@ -87,15 +116,9 @@ net.createServer(function (socket) {
             socket.write(response)
         }
     });
-
-    // On disconnect
-    socket.on('end', function () {
-
-    });
-
 }).listen(5000);
 
-//Crossdomain issues
+//Crossdomain issues - for Unity3D purposes
 net.createServer(function(socket) {
     var crossDomainXml =
         "<?xml version=\"1.0\"?>" +
